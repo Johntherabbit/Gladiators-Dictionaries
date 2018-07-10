1dc476d0b7f47fcb3665ab66d8408e06821e762d
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-10 10:03:10 -0500

    Updated to add 30 rage if a player passes on turn

--- shell.py
+++ shell.py
@@ -12,95 +12,96 @@ def name_of_next():
 
 
 def print_moves():
-    print('1.Attack\n2.Heal\n3.Quit\n')
+    print('~~1.Attack\n~~2.Heal\n~~3.Quit\n~~4.Pass\n')
 
 
-def player_one_fight(name_1, name_2):
+def player_one_fight(player_1, player_2, name_1, name_2):
     while True:
         response = input(
-            'What would you like to do {}? Please use 1,2, or 3 to answer.'.
-            format('Player One'))
+            'What would you like to do {}? Please use 1, 2, 3, or 4 to answer.>>> '.
+            format(name_1))
         print_moves()
         if response == '1':
-            return attack(name_1, name_2)
+            return attack(player_1, player_2)
         elif response == '2':
-            return heal(name_1)
+            return heal(player_1)
         elif response == '3':
             break
+        elif response == '4':
+            player_1['Rage'] = player_1['Rage'] + 30
+            return None
+
+        else:
+            print('Invalid number input.')
 
 
-def player_two_fight(name_1, name_2):
+def player_two_fight(player_1, player_2, name_1, name_2):
     while True:
         response = input(
-            'What would you like to do {}? Please use 1,2, or 3 to answer.'.
-            format('Player 2'))
+            'What would you like to do {}? Please use 1, 2, 3, or 4 to answer.>>> '.
+            format(name_2))
         print_moves()
         if response == '1':
-            return attack(name_2, name_1)
+            attack(player_2, player_1)
         elif response == '2':
-            return heal(name_2)
+            return heal(player_2)
         elif response == '3':
             break
+        elif response == '4':
+            player_2['Rage'] = player_2['Rage'] + 30
+            return None
+        else:
+            print('Invalid number input.')
 
 
-def player_stats(name_1, name_2):
-    print('Player One:\n{}\n{}\n'.format(name_1['Health'], name_1['Rage']))
-    print('Player two:\n{}\n{}\n'.format(name_2['Health'], name_2['Rage']))
+def player_stats(player_1, player_2, name_1, name_2):
+    print('{}:\n{}\n{}\n'.format(name_1, player_1['Health'], player_1['Rage']))
+    print('{}:\n{}\n{}\n'.format(name_2, player_2['Health'], player_2['Rage']))
 
 
-def battle_phase_1(name_1, name_2):
-    player_stats(name_1, name_2)
+def battle_phase_1(player_1, player_2, name_1, name_2):
+    player_stats(player_1, player_2, name_1, name_2)
     print_moves()
-    player_one_fight(name_1, name_2)
+    player_one_fight(player_1, player_2, name_1, name_2)
 
 
-def battle_phase_2(name_1, name_2):
-    player_stats(name_1, name_2)
+def battle_phase_2(player_1, player_2, name_1, name_2):
+    player_stats(player_1, player_2, name_1, name_2)
     print_moves()
-    player_two_fight(name_1, name_2)
+    player_two_fight(player_1, player_2, name_1, name_2)
 
 
-def victor(gladiator, gladiator_2):
+def victor(gladiator, gladiator_2, name_1, name_2):
     while True:
         if is_dead(gladiator) == False and is_dead(gladiator_2) == False:
-            battle_phase_1(gladiator, gladiator_2)
+            battle_phase_1(gladiator, gladiator_2, name_1, name_2)
             if is_dead(gladiator) == True:
-                player_stats(gladiator, gladiator_2)
-                print('Player two wins!')
+                player_stats(gladiator, gladiator_2, name_1, name_2)
+                print("{} wins!".format(name_2))
                 break
             elif is_dead(gladiator_2) == True:
-                player_stats(gladiator, gladiator_2)
-                print('Player one wins')
+                player_stats(gladiator, gladiator_2, name_1, name_2)
+                print("{} wins!".format(name_1))
                 break
-            battle_phase_2(gladiator, gladiator_2)
+            battle_phase_2(gladiator, gladiator_2, name_1, name_2)
             if is_dead(gladiator) == True:
-                player_stats(gladiator, gladiator_2)
-                print('Player two wins!')
+                player_stats(gladiator, gladiator_2, name_1, name_2)
+                print("{} wins!".format(name_2))
                 break
             elif is_dead(gladiator_2) == True:
-                player_stats(gladiator, gladiator_2)
-                print('Player one wins')
+                player_stats(gladiator, gladiator_2, name_1, name_2)
+                print("{} wins!".format(name_1))
                 break
         else:
             victor()
 
-    #while True:
-    #    if is_dead(gladiator) == False and is_dead(gladiator_2) == False:
-    #        battle_phase(gladiator, gladiator_2)
-    #    elif is_dead(gladiator) == True:
-    #        print('Player two wins!')
-    #        break
-    #    elif is_dead(gladiator_2) == True:
-    #        print('Player one wins')
-    #        break
-
 
 def main():
-    #name_1 = name()
-    #name_2 = name_of_next()
-    name_1 = new_gladiator(100, 0, 10, 20)
-    name_2 = new_gladiator(100, 0, 10, 20)
-    victor(name_1, name_2)
+    name_1 = name()
+    name_2 = name_of_next()
+    player_1 = new_gladiator(100, 0, 10, 20)
+    player_2 = new_gladiator(100, 0, 10, 20)
+    victor(player_1, player_2, name_1, name_2)
 
 
 if __name__ == '__main__':
